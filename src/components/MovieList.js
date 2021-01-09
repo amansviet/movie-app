a157713d2a8d3b6bc5adef9657806df4e9f117ea
Amandeep Bhatia <amanbhatia.canada@gmail.com>
2021-01-09 18:36:48 -0500

    Movie-app

---
+++ src/components/MovieList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import AddFavourites from "./AddFavourites";
+const MovieList = (props) => {
+  const FavouriteComponent = props.FavouriteComponent;
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <div className="image-container d-flex justify-content-start m-3">
+          <img src={movie.Poster} alt="movie"></img>
+          <div
+            onClick={() => props.handleFavouriteClick(movie)}
+            className="overlay d-flex align-items-center justify-content"
+          >
+            <FavouriteComponent></FavouriteComponent>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+export default MovieList;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+const SearchBox = (props) => {
+  return (
+    <div className="col col-sm-4">
+      <input
+        className="form-control"
+        onChange={(event) => props.setSearchValue(event.target.value)}
+        placeholder="Type to search"
+      ></input>
+    </div>
+  );
+};
+export default SearchBox;
